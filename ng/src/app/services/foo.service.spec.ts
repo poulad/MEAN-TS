afeba3f828fd427be76594eb0580caed198667e9
Poulad <poulad.pld@gmail.com>
2018-05-27 08:24:59 -0400

    Test foo service completely

--- ng/src/app/services/foo.service.spec.ts
+++ ng/src/app/services/foo.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { FooService } from './foo.service';
 import { ApiResponse } from '../models/api-response';
 import { Foo } from '../models/foo';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('foo service', () => {
 
@@ -17,6 +18,35 @@ describe('foo service', () => {
     });
     afterEach(() => {
         httpTestingController.verify();
+    });
+
+    it('#create should return the new foo from API response', () => {
+        const foo: Foo = {id: '0', bar: 'bar0'};
+        const service = <FooService>TestBed.get(FooService);
+
+        service.create({id: '0', bar: 'bar0'}).subscribe(
+            newFoo => expect(newFoo).toEqual(foo),
+            fail
+        );
+
+        const req = httpTestingController.expectOne('/api/foos');
+        expect(req.request.method).toEqual('POST');
+
+        req.flush(<ApiResponse> {ok: true, data: foo});
+    });
+
+    it('#create should return an error in case of a network failure', () => {
+        const fooService: FooService = TestBed.get(FooService);
+
+        fooService.create({id: '1', bar: 'bar1'}).subscribe(
+            newFoo => fail('expected an error'),
+            (e: HttpErrorResponse) => expect(e.error.message).toContain('test network error')
+        );
+
+        const req = httpTestingController.expectOne('/api/foos');
+        expect(req.request.method).toBe('POST');
+
+        req.error(new ErrorEvent('Network error', {message: 'test network error'}));
     });
 
     it('#getAll should return all foos from API response', () => {
@@ -38,20 +68,15 @@ describe('foo service', () => {
     });
 
     it('#getAll should return an error when web API responds with 404', () => {
-        const errorMessage = 'test network error';
-        const mockError = new ErrorEvent('Network error', {
-            message: errorMessage,
-        });
-
         const service = <FooService>TestBed.get(FooService);
         service.getAll().subscribe(
             foos => fail('expected an error'),
-            e => {
-                expect(e.message).toEqual('test network error');
-            }
+            (e: HttpErrorResponse) => expect(e.error.message).toEqual('test network error')
         );
 
         const req = httpTestingController.expectOne('/api/foos');
         expect(req.request.method).toEqual('GET');
+
+        req.error(new ErrorEvent('Network error', {message: 'test network error'}));
     });
 });
